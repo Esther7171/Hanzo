a0df9c7f93812c89764df2bb8b47437e550cd755
deathesther <122229257+Esther7171@users.noreply.github.com>
2023-12-20 21:09:33 +0530

    Installation.sh

--- hii.sh
+++ hii.sh
@@ -36,11 +36,23 @@ echo ""
     echo -e "\0033[1;34m_______________________________________________\e[0m"
 echo ""
 echo -e "\e[1;32m [*] PLEASE WAIT WHILE WE CHECKING REQUIREMENTS ...\e[0m"
-kihiunhnhin=$(cd /home/$USER/)
-shrhth=$(udo apt-get update -y)
-pojpnmo=$(chmod +x ./hanzo-scan)
-poqrq3u=$(chmod +x ./hanzo-attack)
-tytq3t=$(sudo apt install aircrack-ng -y)
+kihiunhnhin=$(cd /home/$USER/Hanzo)
+shrhth=$(sudo apt-get update -y)
 hngfh=$(sudo apt install mdk4 -y )
 uhkh=$(sudo apt install hcxtools -y)
-echo -e "\033[1;32m[ ✔ ]  ALL DONE "
+swdceercergvy5=$(sudo apt-get install build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre2-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils expect)
+clear
+echo -e "\e[1;34m
+██╗  ██╗ █████╗ ███╗   ██╗███████╗ ██████╗ 
+██║  ██║██╔══██╗████╗  ██║╚══███╔╝██╔═══██╗
+███████║███████║██╔██╗ ██║  ███╔╝ ██║   ██║
+██╔══██║██╔══██║██║╚██╗██║ ███╔╝  ██║   ██║
+██║  ██║██║  ██║██║ ╚████║███████╗╚██████╔╝
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚══════╝ ╚═════╝ \e[0m"
+echo ""
+echo -e "\033[1;35m    +-+-+-+-+-+-+-+-+-+-+-+-+         \033[0m"
+echo -e "\033[1;35m    |D|E|A|T|H| |E|S|T|H|E|R|         \033\[0m"
+echo -e "\033[1;35m    +-+-+-+-+-+-+-+-+-+-+-+-+         \033\[0m"
+echo ""
+echo ""
+echo -e "\033[1;32m[ ✔ ]  INSTALLATION IS COMPLETE... "
